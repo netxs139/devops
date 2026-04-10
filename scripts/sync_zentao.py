d83953150ba31e6e16153bfab3400881e8eba3e6
netxs2000 <netxs2000@163.com>
2026-04-10 17:59:01 +0800

    feat: add session history log and Zentao synchronization script

--- scripts/sync_zentao.py
+++ scripts/sync_zentao.py
@@ -15,6 +15,11 @@ sys.path.append(os.getcwd())
 from sqlalchemy.orm import configure_mappers
 
 
+from devops_collector.core.plugin_loader import PluginLoader
+
+# 强制加载所有插件模型，确保 SQLAlchemy Mapper 能解析全部跨插件 relationship
+PluginLoader.load_models()
+
 try:
     configure_mappers()
 except Exception:
